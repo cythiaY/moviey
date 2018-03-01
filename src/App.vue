71cc5019f485a79a4d342caab3fb63a80969d826
summer_y <cythia.xy@gmail.com>
2018-03-01 09:32:47 +0800

    import ‘UIkit’ frame

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@ export default {
 </script>
 
 <style>
+@import "static/commom/uikit.min.css";
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/components/login.vue
+++ src/components/login.vue
@@ -6,6 +6,9 @@
     <div class="form_item">
       <input type="password" name="" id="" placeholder="密码">
     </div>
+    <div class="form_item">
+      <button class="uk-button uk-button-secondary">登录</button>
+    </div>
   </div>
 </template>
 
