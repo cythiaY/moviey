d23d66eec168f21349d44558a21dacf59491f8cd
summer_y <cythia.xy@gmail.com>
2018-03-01 18:09:43 +0800

    repair the login api

--- src/components/login.vue
+++ src/components/login.vue
@@ -1,16 +1,18 @@
 <template>
-  <div class="container">
+  <div class="container" :style="{backgroundImage: 'url('+bgImg+')'}">
     <div class="forms">
       <form class="loginForm" :model="loginForm">
-        <div class="uk-margin">
+        <div class="floatR">
           <input type="text" class="uk-input uk-width-2-5" v-model="loginForm.username" placeholder="用户名">
         </div>
         <div class="uk-margin">
           <input type="password" class="uk-input uk-width-2-5" name="" id="" v-model="loginForm.password" placeholder="密码">
         </div>
         <div class="uk-margin">
-          <button class="uk-button uk-button-secondary" @click="login">登录</button>
-          <button class="uk-button uk-button-link">忘记密码</button>
+          <div class="uk-width-2-5" style="margin-left:30%">
+            <button class="uk-button uk-button-small uk-button-secondary uk-align-left" @click="login">登录</button>
+            <button class="uk-button uk-button-link uk-align-right" style="line-height:40px">忘记密码</button>
+          </div>
         </div>
       </form>
       <form class="uk-width-1-2">
@@ -24,6 +26,7 @@
           <button class="uk-button uk-button-danger">注册</button>
         </div>
       </form>
+      <div style="clear:both"></div>
     </div>
   </div>
 </template>
@@ -33,17 +36,32 @@ export default {
   name: 'login',
   data () {
     return {
-      loginForm: {}
+      loginForm: {},
+      bgImg: require('../../src/static/images/login_bg.jpeg'),
+      isloginName: false
     }
   },
   methods: {
     login () {
-      this.$http.post('http://localhost:8087/user/add', {userName: 'cdc', userPassword: '123456'}, {emulateJSON: true}).then(response => {
-        console.log(response)
-      }, response => {
-        console.log('error')
-      })
-      console.log(this.loginForm.username)
+      var tag = true
+      if (this.loginForm.username === '' || this.loginForm.username === null) {
+        tag = false
+        this.isloginName = true
+      }
+      if (this.loginForm.userPassword === '' || this.loginForm.userPassword === null) {
+        tag = false
+      }
+      if (tag) {
+        var data = {
+          userName: this.loginForm.username,
+          userPassword: this.loginForm.userPassword
+        }
+        this.$http.get('http://localhost:8087/user/login', {params: data}, {emulateJSON: true}).then(response => {
+          console.log(response.data)
+        }, response => {
+          console.log('登录失败～')
+        })
+      }
     }
   }
 }
