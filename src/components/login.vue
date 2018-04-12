41d3a2a262bc4799a93b8fd30c64b9eca15392e7
summer_y <cythia.xy@gmail.com>
2018-04-12 17:04:41 +0800

    repair login

--- src/components/login.vue
+++ src/components/login.vue
@@ -8,7 +8,7 @@
           <el-input v-model="loginForm.username" placeholder="登录用户名" @blur="checkUserName"></el-input>
         </el-form-item>
         <el-form-item label="" prop="password">
-          <el-input v-model="loginForm.password" type="password" placeholder="登录密码" @blur="checkUserPassord"></el-input>
+          <el-input v-model="loginForm.password" type="password" placeholder="登录密码" @blur="checkUserPassord" @keyup.enter.native="login"></el-input>
         </el-form-item>
         <el-form-item>
           <button class="leftBtn" @click="login">{{loginState}}</button>
@@ -102,7 +102,6 @@
               setCookie('id', response.data, 1000 * 60 * 60)
               if (response.data && response.data !== 0) {
                 this.$router.push({ path: '/index' })
-                console.log('aaa')
               } else {
                 this.$message.error('用户名密码错误，请重新输入')
               }
