ecbd8803a8fca44814bd5ee95c9fa9ada747701a
summer_y <cythia.xy@gmail.com>
2018-04-14 11:30:36 +0800

    change color

--- src/components/crm/login.vue
+++ src/components/crm/login.vue
@@ -34,7 +34,7 @@
           username: [
             { required: true, message: '请输入用户名', trigger: 'blur' }
           ],
-          password: [{ required: true, message: '请选择密码', trigger: 'blur' }]
+          password: [{ required: true, message: '请输入密码', trigger: 'blur' }]
         }
       }
     },
